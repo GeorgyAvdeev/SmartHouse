cd99da438af0c05ec173f9bfd0e59a3dce0a0e47
blaizzz.avd@mail.ru <blaizzz.avd@mail.ru>
2020-01-19 20:19:36 +0300

    add 7 buttons

--- main.py
+++ main.py
@@ -12,20 +12,34 @@ def Up(event):
 
 def Down(event):
     global selection_number
-    if selection_number < 2:
+    if selection_number < 9:
         selection_number += 1
 
 
 window = Tk()
 
-canvas = Canvas(window, width=400, height=300, bg='green')
+canvas = Canvas(window, width=400, height=1000, bg='green')
 UI1 = UIElement("temperature = 30°C", Coords(0, 0), 400, 100, "", canvas)
 
 UI2 = UIElement("light on", Coords(0, 100), 400, 100, "", canvas)
 
 UI3 = UIElement("settings", Coords(0, 200), 400, 100, "", canvas)
 
-UI = [UI1, UI2, UI3]
+UI4 = UIElement("4", Coords(0, 300), 400, 100, "", canvas)
+
+UI5 = UIElement("5", Coords(0, 400), 400, 100, "", canvas)
+
+UI6 = UIElement("6", Coords(0, 500), 400, 100, "", canvas)
+
+UI7 = UIElement("7", Coords(0, 600), 400, 100, "", canvas)
+
+UI8 = UIElement("8", Coords(0, 700), 400, 100, "", canvas)
+
+UI9 = UIElement("9", Coords(0, 800), 400, 100, "", canvas)
+
+UI10 = UIElement("10", Coords(0, 900), 400, 100, "", canvas)
+
+UI = [UI1, UI2, UI3, UI4, UI5, UI6, UI7, UI8, UI9, UI10]
 
 canvas.pack()
 
@@ -38,4 +52,11 @@ while True:
     UI1.render()
     UI2.render()
     UI3.render()
+    UI4.render()
+    UI5.render()
+    UI6.render()
+    UI7.render()
+    UI8.render()
+    UI9.render()
+    UI10.render()
     UI[selection_number].select(False)
